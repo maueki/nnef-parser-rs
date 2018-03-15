98855f766d15b629e9237b4d663eee39a4867b3d
Ueki Masaru <masaru.ueki@itage.co.jp>
2018-03-15 15:38:20 +0900

    Add id_list()

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,28 @@
 #[macro_use]
 extern crate combine;
 
-use combine::parser::char::{letter, char, alpha_num};
+use combine::parser::char::{letter, char, alpha_num, spaces};
 use combine::stream::{Stream};
 
 use combine::*;
 
-#[derive(Debug, PartialEq)]
-pub enum Identifier {
-    Id(String),
-}
-
-parser!{
-    fn identifier[I]()(I) -> Identifier
+parser! {
+    fn identifier[I]()(I) -> String
         where [I: Stream<Item=char>]
     {
         (letter().or(char('_')),
          many(alpha_num().or(char('_'))))
-            .map(|(t1, t2): (char, String)| Identifier::Id(t1.to_string() + t2.as_str()))
+            .map(|(t1, t2): (char, String)| t1.to_string() + t2.as_str())
     }
 }
 
+parser! {
+    fn id_list[I]()(I) -> Vec<String>
+        where [I: Stream<Item=char>]
+    {
+        sep_by(spaces().with(identifier().skip(spaces())), token(','))
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -32,11 +34,16 @@ mod tests {
 
     #[test]
     fn id_test() {
-        assert_eq!(identifier().parse("a"), Ok((Identifier::Id("a".to_string()), "")));
-        assert_eq!(identifier().parse("abc12"), Ok((Identifier::Id("abc12".to_string()), "")));
-        assert_eq!(identifier().parse("_12"), Ok((Identifier::Id("_12".to_string()), "")));
+        assert_eq!(identifier().parse("a"), Ok(("a".to_string(), "")));
+        assert_eq!(identifier().parse("abc12"), Ok(("abc12".to_string(), "")));
+        assert_eq!(identifier().parse("_12"), Ok(("_12".to_string(), "")));
 
         assert_eq!(identifier().parse("123"), Err(UnexpectedParse));
         assert_eq!(identifier().parse(""), Err(UnexpectedParse));
+    }
+
+    fn id_list_test() {
+        assert_eq!(id_list().parse("a"), Ok((vec!["a".to_string()], "")));
+        assert_eq!(id_list().parse(" a, b "), Ok((vec!["a".to_string(), "b".to_string()], "")));
     }
 }
